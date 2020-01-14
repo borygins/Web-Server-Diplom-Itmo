f8ecb84adfbc0dd796a02e13325b85418fda27ca
xmitya <xmitya@gmail.com>
2020-01-14 21:03:14 +0300

    Fix test

--- server/src/test/java/ru/ifmo/server/SuccessHandler.java
+++ server/src/test/java/ru/ifmo/server/SuccessHandler.java
@@ -13,8 +13,9 @@ public class SuccessHandler implements Handler {
 
     @Override
     public void handle(Request request, Response response) throws Exception {
-        response.getOutputStream().write((OK_HEADER + TEST_RESPONSE +
-                "<br>" + request.getArguments() + CLOSE_HTML).getBytes());
+        response.getWriter().write(TEST_RESPONSE);
+        response.getWriter().write("<br>" + request.getArguments());
+        response.getWriter().write(CLOSE_HTML);
         response.getOutputStream().flush();
     }
 }
